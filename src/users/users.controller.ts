3cb769f1559d73e35bf17a7dadd9a020c09d71c9
yaser-alazm <yaser.azem@gmail.com>
2025-07-15 15:50:15 +0200

    cleaning

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,12 +7,15 @@ import {
   Put,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
-@Controller('users')
+@Controller()
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
